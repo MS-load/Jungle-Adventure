49be8f52777bca292ec24ebe003a3742845a21ce
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-10-27 00:44:49 +0200

    Scenes and IF

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let rooms = {
-    "room0": "You are in the jungle"
+let scenes = {
+    "scene0": "You are in the jungle",
+    "scene1": "You are in the stage1",
+    "scene2": "You are in the stage2",
+    "scene3": "You are in the stage3",
+    "scene4": "You are in the stage4",
+    "scene5": "You are in the stage5",
+    "scene6": "You are in the stage6",
+    "scene7": "You are in the stage7",
+    "scene8": "You are in the stage8",
+    "scene9": "You are in the stage9",
 }
 
 
@@ -7,6 +16,16 @@ let rooms = {
 function getUserInput(){
     let gameText = document.getElementById("gameText")
     let userInput = document.querySelector(".form-control")
-    gameText.append(rooms.room0)
     console.log(userInput.value)
+
+    if(userInput.value == "yes"){
+        gameText.append("Great then let's get started"+ scenes["scene"+0])
+
+    }
+    else if (userInput.value == "no"){
+        gameText.append("Sorry to see you go!")
+    }
+    else{
+        gameText.append("That is not a valid option")
+    }
 }
