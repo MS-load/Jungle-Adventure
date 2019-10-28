61e912406c13ff3f469a3d6253dbff294280f8d7
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-10-28 19:11:08 +0100

    choices & scenes first stage

--- script.js
+++ script.js
@@ -1,62 +1,175 @@
-/**This is a description of the userWantsToPlay function.
- * Creates homepage if the user wants to play!
- */
 
+/**This function confirms the user wants to play*/
 function userWantsToPlay() {
 
     alert("Great then let's get started")
-    const name = prompt("What's your name?")
-    let welcome = document.getElementById("welcome")
-    welcome.innerText = "Hello " + name + ", " + welcome.innerText //Adding the user name to the welcome message
 
+    /** @const {string} name gets the players name */
+    const name = prompt("What's your name?")
+
+    //Adding the player name to the welcome message
+    let welcome = document.getElementById("welcome")
+    welcome.innerText = "Hello " + name + ", \n" + welcome.innerText
+
+    //Setting up the playing field
     let hide = document.querySelector("main .btn")
     hide.style.display = "none"
-
     let unHide = document.querySelector("footer")
     unHide.classList.remove("d-none")
 
+    //------------------PLAYER BEGINS THE GAME-----------
     let gameText = document.getElementById("gameText")
-    gameText.innerText = "Some text here. \n" + scenes["scene" + 0]//Start scene first crossroad the player arrives at 
+    gameText.innerText = "Jungles form a magical place, and you find your self admist one\n" + scenes["scene" + 0].discription
+
 }
+
+
+
 
 
 //Object scenes decribes the various scenarios that the user faces
 let scenes = {
-    "scene0": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-    "scene1": "You are in the stage1",
-    "scene2": "You are in the stage2",
-    "scene3": "You are in the stage3",
-    "scene4": "You are in the stage4",
-    "scene5": "You are in the stage5",
-    "scene6": "You are in the stage6",
-    "scene7": "You are in the stage7",
-    "scene8": "You are in the stage8",
-    "scene9": "You are in the stage9",
+    "scene0": {
+        "discription": ("You arrive at a cross road where .... which way would you go left right or straight? "),
+        "choices": {
+            "left": "scene3",
+            "right": "scene1",
+            "straight": "scene2",
+        }
+    },
+    "scene1": {
+        "discription": "elephant herd ....." + +"what do you choose to do attack or avoid ?",
+        "choices": {
+            "attack": "scene9",
+            "avoid": "scene5"
+        }
+    },
+    "scene2": {
+        "discription": "Hungry you find a tree bearing beautiful furits .....what do you choose to do eat or skip?",
+        "choices": {
+            "eat": "scene9",
+            "skip": "scene4"
+        }
+    },
+    "scene3": {
+        "discription": "See a small stream .....what do you choose to do jump or walk along ?",
+        "choices": {
+            "jump": "scene9",
+            "walk": "scene6"
+        }
+    },
+    "scene4": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
+    "scene5": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
+    "scene6": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
+    "scene7": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
+    "scene8": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
+    "scene9": {
+        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
+        "choices": {
+            "north": "scene1",
+            "south": "scene2"
+        }
+    },
 }
 
+//Object items decribes the various things that the user can carry
+let items = {
+    "item1": "Binocuars",
+    "item2": "Rope",
+    "item3": "Water Bottle",
+    "item4": "Boat",
+    "item5": "logs"
+}
 
-/**This is a description of the getUserInput function.
- * @param {string} userInput gives the users response after converting it to lowercase
- * @returns {string} based on condiiton
+//Object actions decribes the various actions that the user can perform
+let actions = {
+    "look up": "The sun is shining and its a really hot day ...",
+    "pick item": "You've picked it but remember not to carry too mnay things its a long walk",
+    "jump": "Thats was a huge leap",
+    "climb": "That was tiring climbing all the way up here ",
+    "run": "Phew! close call! Good thing that you were quick",
+}
+
+// function scene(sceneNumber, description, direction) {
+//     this.sceneNumber = sceneNumber;
+//     this.description = description;
+//     this.direction = direction;
+
+//     updateScene(){
+
+//     }
+// }
+
+// let changeScenes = [
+//     new scene (01,"Jungles form a magical place, and you find your self admist one .....what do you choose to do ?" )
+// ]
+
+/**This function records the players input
+ * @param  {string} userInput gives the users response after converting it to lowercase
  */
-
-function getUserInput(userInput) {
+function getPlayerInput(playerInput) {
     let gameText = document.getElementById("gameText")
-    let getUserInput = document.querySelector(".form-control").value
-    userInput = getUserInput.toLowerCase()
+    let getPlayerInput = document.querySelector(".form-control").value
+    playerInput = getPlayerInput.toLowerCase()
+    // playerChoice()
 
-    if (userInput == "yes") {
-        gameText.append("Great then let's get started" + scenes["scene" + 0])
+    switch (playerInput) {
+        case "left":
+            changeScene("left")
+            break;
+        case "right": changeScene("right")
+            break;
+        case "straight": changeScene("straight")
+            break;
+        case "back": changeScene("back")
+            break;
+        default:
+            alert("Im sorry, I do not undetsand what you are trying to do. That's not really an option you have!")
+    }
 
-    }
-    else if (userInput == "no") {
-        gameText.append("Sorry to see you go!")
-    }
-    else {
-        gameText.append("That is not a valid option")
+
+}
+/**This is fuction allows us to call the getUserInput funtion on pressing the Enter key
+ */
+document.onkeydown = function () {
+    if (window.event.keyCode == '13') {
+        getPlayerInput();
     }
 }
 
-function chnagescenes(direction) {
+let start = "scene0"
 
+function changeScene(action) {
+    start = scenes[start].choices[action]
+    gameText.append("\n" + scenes[start].discription)
 }
