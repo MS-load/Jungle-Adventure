d5cb0a8ae0143ab44be2f5d31901588b14255e95
Mamta Srinivas <mamtasrinivassp@gmail.com>
2019-10-28 23:15:32 +0100

    Improved scenes structure

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
 /**This function confirms the user wants to play*/
 function userWantsToPlay() {
-
     alert("Great then let's get started")
+    let playerName = prompt("What's your name?")
+    playingField(playerName)
+}
 
-    /** @const {string} name gets the players name */
-    const name = prompt("What's your name?")
-
-    //Adding the player name to the welcome message
+/**This function sets the playing field
+ * @param {string} name adds player name to the welcome text 
+*/
+function playingField(name) {
     let welcome = document.getElementById("welcome")
     welcome.innerText = "Hello " + name + ", \n" + welcome.innerText
 
-    //Setting up the playing field
     let hide = document.querySelector("main .btn")
     hide.style.display = "none"
     let unHide = document.querySelector("footer")
@@ -19,88 +20,38 @@ function userWantsToPlay() {
 
     //------------------PLAYER BEGINS THE GAME-----------
     let gameText = document.getElementById("gameText")
-    gameText.innerText = "Jungles form a magical place, and you find your self admist one\n" + scenes["scene" + 0].discription
+    gameText.innerText = "Jungles form a magical place, and you find your self admist one\n" + scene0.discription
 
 }
 
 
+//----------------GAME SET UP-------------------
 
-
-
-//Object scenes decribes the various scenarios that the user faces
-let scenes = {
-    "scene0": {
-        "discription": ("You arrive at a cross road where .... which way would you go left right or straight? "),
-        "choices": {
-            "left": "scene3",
-            "right": "scene1",
-            "straight": "scene2",
-        }
-    },
-    "scene1": {
-        "discription": "elephant herd ....." + +"what do you choose to do attack or avoid ?",
-        "choices": {
-            "attack": "scene9",
-            "avoid": "scene5"
-        }
-    },
-    "scene2": {
-        "discription": "Hungry you find a tree bearing beautiful furits .....what do you choose to do eat or skip?",
-        "choices": {
-            "eat": "scene9",
-            "skip": "scene4"
-        }
-    },
-    "scene3": {
-        "discription": "See a small stream .....what do you choose to do jump or walk along ?",
-        "choices": {
-            "jump": "scene9",
-            "walk": "scene6"
-        }
-    },
-    "scene4": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
-    "scene5": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
-    "scene6": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
-    "scene7": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
-    "scene8": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
-    "scene9": {
-        "discription": "Jungles form a magical place, and you find your self admist one .....what do you choose to do ?",
-        "choices": {
-            "north": "scene1",
-            "south": "scene2"
-        }
-    },
+function controls(walk,choice,look,help,pick){
+ this.walk = walk;
+ this.choice = choice;
+ this.look = look;
+ this.help = help;
+ this.pick = pick;
 }
+
+function scene(description,control,pick){
+    this.description = description;
+    this.control = control;
+    this.pick = pick;
+}
+
+let scene0 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene1 = new scene ("elephant herd .....what do you choose to do attack or avoid ?","choice")
+let scene2 = new scene ("Hungry you find a tree bearing beautiful furits .....what do you choose to do eat or skip?","choice")
+let scene3 = new scene ("See a small stream .....what do you choose to do jump or walk along ?","walk")
+let scene4 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene5 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene6 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene7 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene8 = new scene ("You arrive at a cross road where .... which way would you go left right or straight?","walk")
+let scene9 = new scene ("DEAD GAME OVER")
+let scene10 = new scene ("YOU WON")
 
 //Object items decribes the various things that the user can carry
 let items = {
@@ -141,23 +92,7 @@ function getPlayerInput(playerInput) {
     let gameText = document.getElementById("gameText")
     let getPlayerInput = document.querySelector(".form-control").value
     playerInput = getPlayerInput.toLowerCase()
-    // playerChoice()
-
-    switch (playerInput) {
-        case "left":
-            changeScene("left")
-            break;
-        case "right": changeScene("right")
-            break;
-        case "straight": changeScene("straight")
-            break;
-        case "back": changeScene("back")
-            break;
-        default:
-            alert("Im sorry, I do not undetsand what you are trying to do. That's not really an option you have!")
-    }
-
-
+    playerChoice(playerInput)
 }
 /**This is fuction allows us to call the getUserInput funtion on pressing the Enter key
  */
@@ -165,6 +100,25 @@ document.onkeydown = function () {
     if (window.event.keyCode == '13') {
         getPlayerInput();
     }
+}
+
+function playerChoice(value){
+    let command = value.split(" ")[0]
+     switch (command) {
+        case "go":
+            let action = value.split(" ")[1]
+            changeScene(action)
+            break;
+        // case "right": changeScene("right")
+        //     break;
+        // case "straight": changeScene("straight")
+        //     break;
+        // case "back": changeScene("back")
+        //     break;
+        default:
+            alert("I'm sorry, but that's not really an option you have!")
+    }
+
 }
 
 let start = "scene0"
